6054052d8527ce8facaa14e7676a95e3cc05256e
ferdynandpro <93565401+ferdynandpro@users.noreply.github.com>
2025-01-05 22:21:29 +0700

    first commit

--- index.js
+++ index.js
@@ -4,33 +4,28 @@ import dotenv from "dotenv";
 import { errorHandler } from "./middlewares/ErrorHandler.js";
 import AuthRoutes from "./routes/AuthRoutes.js";
 import CustomerRoutes from "./routes/CustomerRoutes.js";
-import ProductRoutes from "./routes/ProductRoutes.js"; // Import Product Routes
-import DiscountRoutes from "./routes/DiscountRoutes.js"; // Import Discount Routes
+import ProductRoutes from "./routes/ProductRoutes.js";
+import DiscountRoutes from "./routes/DiscountRoutes.js";
 import logsRoute from './routes/LogsRoutes.js';
 
 dotenv.config();
 
 const app = express();
 
-// Middleware for parsing JSON bodies
 app.use(express.json());
 
-// CORS Configuration
 app.use(cors({
-  origin: "http://localhost:3000",  // Adjust to your frontend URL
+  origin: "http://localhost:3000",
   methods: "GET,POST,PUT,DELETE",
   allowedHeaders: "Content-Type,Authorization"
 }));
 
-// Routes
-
 app.use('/api', logsRoute);
 app.use("/api", AuthRoutes);
 app.use("/api", CustomerRoutes);
-app.use("/api", ProductRoutes);  // Use Product Routes
-app.use("/api", DiscountRoutes);  // Use Discount Routes
+app.use("/api", ProductRoutes);  
+app.use("/api", DiscountRoutes); 
 
-// Error handler middleware
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 4000;
